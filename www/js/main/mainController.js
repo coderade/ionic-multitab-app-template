dba6466156ea0c544d23729397eb365439fe7748
Valdeci Gomes <valdeci@coderade.in>
2016-08-16 00:29:51 -0300

    add global header

--- www/js/main/mainController.js
+++ www/js/main/mainController.js
@@ -5,17 +5,19 @@ ionicApp.controller("mainController", ['$scope', '$http', '$state',
         $scope.artists = data;
         $scope.whichArtist =$state.params.aId;
 
+        $scope.data = {showDelete: false,  showReorder: false};
+
         $scope.moveArtist = function (artist, fromIndex, toIndex) {
           $scope.artists.splice(fromIndex, 1);
           $scope.artists.splice(toIndex, 0, artist);
         };
 
-        $scope.onArtistDelete=  function (game) {
-          $scope.artists.splice($scope.artists.indexOf(game), 1)
+        $scope.onArtistDelete=  function (artist) {
+          $scope.artists.splice($scope.artists.indexOf(artist), 1)
         };
 
-        $scope.toggleStar = function (game) {
-          game.star = !game.star;
+        $scope.toggleStar = function (artist) {
+          artist.star = !artist.star;
         };
 
         $scope.doRefresh = function () {
